d678ce037df937629efd9cb55e4334939a24e883
ZiheLiu <272910663@qq.com>
2019-08-20 15:46:13 +0800

    Use CAS to set backendCtx

--- frontend/src/main/java/com/ziheliu/FrontendCreateChannelHandler.java
+++ frontend/src/main/java/com/ziheliu/FrontendCreateChannelHandler.java
@@ -8,26 +8,28 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class FrontendCreateChannelHandler extends ChannelInboundHandlerAdapter {
-  private static ChannelHandlerContext backendCtx;
+  private static AtomicReference<ChannelHandlerContext> backendCtxRef = new AtomicReference<>(null);
 
   public static void send(RpMessage msg) {
     System.out.println("<FrontendCreateChannelHandler send>: " + msg.getPort());
-    backendCtx.write(msg);
+    backendCtxRef.get().write(msg);
   }
 
   public static void flush() {
-    backendCtx.flush();
+    backendCtxRef.get().flush();
   }
 
   public static boolean connected2backend() {
-    return backendCtx != null;
+    return backendCtxRef.get() != null;
   }
 
   @Override
   public void channelActive(ChannelHandlerContext ctx) throws Exception {
-    if (backendCtx != null) {
+    ChannelHandlerContext backendCtx = backendCtxRef.get();
+    if (backendCtx!= null) {
       if (!backendCtx.equals(ctx)) {
         ctx
           .writeAndFlush("Frontend already connected to a backend.")
@@ -35,23 +37,24 @@ public class FrontendCreateChannelHandler extends ChannelInboundHandlerAdapter {
       }
     } else {
       System.out.println("Create Channel");
-      backendCtx = ctx;
-      ctx.pipeline()
-        .addFirst(new RpEncoder())
-        .addFirst(new LengthFieldPrepender(4))
+      if (backendCtxRef.compareAndSet(null, ctx)) {
+        ctx.pipeline()
+          .addFirst(new RpEncoder())
+          .addFirst(new LengthFieldPrepender(4))
 
-        .addLast(new LengthFieldBasedFrameDecoder(60 * 1024, 0, 4, 0, 4))
-        .addLast(new RpDecoder())
-        .addLast(new BackendReadHandler());
+          .addLast(new LengthFieldBasedFrameDecoder(60 * 1024, 0, 4, 0, 4))
+          .addLast(new RpDecoder())
+          .addLast(new BackendReadHandler());
+      }
     }
   }
 
   @Override
   public void channelInactive(ChannelHandlerContext ctx) throws Exception {
     ctx.close();
-    if (backendCtx.equals(ctx)) {
+    if (backendCtxRef.get().equals(ctx)) {
       System.out.println("Close Channel");
-      backendCtx = null;
+      backendCtxRef.set(null);
     }
   }
 }
